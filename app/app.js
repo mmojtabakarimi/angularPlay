076e1eb05240b09ef015a88090090d6a82ae7c06
mmojtabakarimi <mmojtabakarimi@yahoo.com>
2018-08-05 01:36:50 +0430

    second

--- app/app.js
+++ app/app.js
@@ -1,48 +1,105 @@
-var  mkmApp = angular.module('mkmApp',[]);
+var  mkmApp = angular.module('mkmApp',['ngRoute']);
 
-mkmApp.config(function(){
+mkmApp.config(['$routeProvider',function($routeProvider){
 
+  $routeProvider
+    .when('/home',{
+      templateUrl: 'views/home.html'
+    })
+    .when('/directory',{
+      templateUrl: 'views/directory.html',
+      controller :  'mojtabaController'
+    })
+    .otherwise({
+        redirectTo:'/home'
+    });
 
+}]);
 
-});
 
 mkmApp.run(function(){
 
 
 });
 
-mkmApp.controller('mojtabaController',function($scope){
+mkmApp.controller('mojtabaController',function($scope, $route, $routeParams, $location){
+
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+
+    $scope.removeLine = function(p)
+    {
+      var  removePerson = $scope.persons.indexOf(p);
+      $scope.persons.splice(removePerson,1);
+
+
+    };
+
+    $scope.watch = function() {
+        console.log( "salalalal");
+        console.log( $location.path());
+
+    };
+
+    $scope.addNewPerson = function()
+    {
+      $scope.persons.push(
+        {
+          name: $scope.newPerson.name ,
+          belt: $scope.newPerson.belt,
+          rate:parseInt( $scope.newPerson.rate),
+          availabel: true
+        });
+        $scope.newPerson.name ="";
+        $scope.newPerson.belt ="";
+        $scope.newPerson.rate ="";
+
+
+    };
+
 
     $scope.message="Salam Bar  angular";
     $scope.fruits=['k1','k2','ff','gg','ddssdsa'];
     $scope.persons=[
       {
         name:"ali",
-        belt:"yello",
-        rate:100
+        belt:"yellow",
+        rate:100,
+        availabel:true,
+        thumb: "content/img/1.png"
       },
       {
         name:"sharam",
         belt:"blue",
-        rate:80
+        rate:80,
+        availabel:false,
+        thumb: "content/img/2.png"
+
       },
 
       {
         name:"payam",
         belt:"green",
-        rate:500
+        rate:500,
+        availabel:true,
+        thumb: "content/img/3.png"
       },
 
       {
         name:"Timmy",
         belt:"red",
-        rate:20
+        rate:20,
+        availabel:false,
+        thumb: "content/img/4.png"
       },
 
       {
         name:"Young",
         belt:"brown",
-        rate:10
+        rate:10,
+        availabel:true,
+        thumb: "content/img/1.png"
       }
 
     ];
